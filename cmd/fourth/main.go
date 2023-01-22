5c1fac9cd85e518c84d8f5639b850ae1b37da1f7
Sergio Ponomarev <ponomarevesq@gmail.com>
2023-01-23 02:56:41 +0400

    refactored project

--- cmd/fourth/main.go
+++ cmd/fourth/main.go
@@ -1,55 +1,13 @@
 package main
 
 import (
-	"context"
-	"errors"
-	"fmt"
-    "fourth/pkg/consts"
-	"io"
+	"fourth/internal/app/endpoint"
+	"fourth/pkg/consts"
 	"log"
-	"net"
-	"net/http"
 )
 
 func main() {
-    log.Println(consts.SrvStrd)
+	log.Println(consts.SrvStrd)
 
-	ListeningStart()
-}
-
-func ListeningStart() {
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/hello", getHello)
-
-	ctx := context.Background()
-	server := &http.Server{
-		Addr:    ":3333",
-		Handler: mux,
-		BaseContext: func(l net.Listener) context.Context {
-            ctx = context.WithValue(ctx, consts.KeySvrAddr, l.Addr().String())
-			return ctx
-		},
-	}
-
-	err := server.ListenAndServe()
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("server closed\n")
-	} else if err != nil {
-		fmt.Printf("error listening for server: %s\n", err)
-	}
-}
-
-func getHello(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-
-	first := r.URL.Query().Get("first")
-	second := r.URL.Query().Get("second")
-
-	log.Printf("%s: got / request. first=%s, second=%s\n",
-        ctx.Value(consts.KeySvrAddr),
-		first,
-		second)
-
-	io.WriteString(w, "This is my website!\n")
+	endpoint.New()
 }
